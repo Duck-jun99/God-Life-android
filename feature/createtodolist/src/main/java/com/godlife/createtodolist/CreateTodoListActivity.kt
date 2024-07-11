f37ec05354914f220f0536c4ba7610b43de3217f
Duck-jun99 <byoungjun320@gmail.com>
2024-07-12 01:31:07 +0900

    add: 뒤로가기 시 AlertDialog 추가

--- feature/createtodolist/src/main/java/com/godlife/createtodolist/CreateTodoListActivity.kt
+++ feature/createtodolist/src/main/java/com/godlife/createtodolist/CreateTodoListActivity.kt
@@ -2,27 +2,36 @@ package com.godlife.createtodolist
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.godlife.designsystem.component.GodLifeButton
+import com.godlife.designsystem.component.GodLifeButtonWhite
 import com.godlife.designsystem.theme.GodLifeTheme
+import com.godlife.designsystem.theme.GrayWhite
+import com.godlife.designsystem.theme.PurpleMain
 import com.godlife.navigator.MainNavigator
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -33,10 +42,49 @@ class CreateTodoListActivity :ComponentActivity() {
     @Inject
     lateinit var mainNavigator: MainNavigator
 
+    private var showExitDialog by mutableStateOf(false)
+
+    //콜백 인스턴스 생성
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+
+            // 뒤로 버튼 이벤트 처리
+            showExitDialog = true
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        this.onBackPressedDispatcher.addCallback(this, callback)
+
         setContent{
+            if (showExitDialog) {
+                AlertDialog(
+                    containerColor = Color.White,
+                    onDismissRequest = { showExitDialog = false },
+                    title = {
+                        Text(text = "투두 리스트 제작을 종료할까요?", style = TextStyle(color = PurpleMain, fontSize = 18.sp, fontWeight = FontWeight.Bold))
+                    },
+                    text = {
+                        Text(text = "아직 오늘의 투두 리스트가 완성되지 않았어요.", style = TextStyle(color = GrayWhite, fontSize = 15.sp, fontWeight = FontWeight.Normal))
+                    },
+                    confirmButton = {
+                        GodLifeButtonWhite(
+                            onClick = { mainNavigator.navigateFrom(this, withFinish = true) },
+                            text = { Text(text = "종료하기", style = TextStyle(color = PurpleMain, fontSize = 18.sp, fontWeight = FontWeight.Bold)) }
+                        )
+                    },
+                    dismissButton = {
+                        GodLifeButtonWhite(
+                            onClick = { showExitDialog = false },
+                            text = { Text(text = "취소", style = TextStyle(color = PurpleMain, fontSize = 18.sp, fontWeight = FontWeight.Bold)) }
+                        )
+                    }
+                )
+            }
+
             CreateTodoList(mainNavigator, this)
         }
 
